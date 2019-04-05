5798edcf41f91dcd06510af14e13b7f8b3dbcbe1
ryleydavenport <31938121+ryleydavenport@users.noreply.github.com>
2019-04-04 21:42:24 -0400

    Updated the user updating

---
+++ viewaccount.js
@@ -0,0 +1,15 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    var name, email;
+    if (user) {
+        // User is signed in.
+        name = user.displayName;
+        email = user.email;
+        password = user.password;
+        document.getElementById("name").innerHTML = name;
+        document.getElementById("email").innerHTML = email;
+        
+    } else {
+        // No user is signed in.
+
+    }
+});
